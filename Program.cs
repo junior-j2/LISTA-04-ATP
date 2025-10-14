4c1dce12c15645afcdc81d9193eda7b1bfdc0f19
junior-j2 <juninhoantunessene@hotmail.com>
2025-10-14 10:15:50 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+/*A potenciação é uma operação matemática que representa o processo de multiplicar um
+número por ele mesmo várias vezes, usando um expoente ou índice. Ela é frequentemente
+expressa como "a elevado à potência de n" e é escrita na forma a^n, onde "a" é a base e "n"
+é o expoente. O resultado da operação é chamado de potência. Diante disso, escreva uma
+função que receba dois números: base e expoente e calcule a base elevada ao expoente.
+Não utilize a função Math.Pow.*/
+
+class Questão1
+{
+    public static void roda()
+    {
+        Console.WriteLine("Digite a base: ");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite o expoente: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine("O resultado de " + a + " elevado a " + n + " é: " + Potencia(a, n));
+    }
+    static int Potencia(int a, int n)
+    {
+        int m = 1;
+        for (int x = 0; x < n; x++)
+            m = m * a;
+        return m;
+    }
+    public static void Main()
+    {
+        roda();
+    }
+}
